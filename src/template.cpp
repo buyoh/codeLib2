ecf5c6be0909b4db869c7b558d742bd9eea2881c
buyoh <japanjetone@live.jp>
2017-11-29 22:21:46 +0900

    readmeと分類予定の空のファイルを追加

---
+++ src/template.cpp
@@ -0,0 +1,18 @@
+// %title
+// algorithm name here
+// 
+// %overview
+// my nice algorithm.
+//
+// %usage
+// int func(int x);
+// x : input
+// @ret
+//
+// %verified
+// problem.
+// 
+// %references
+// thanks.
+
+int func(int x){}
