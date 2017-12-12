e6c750dfeeef42edbf012477866cbec335bf2794
buyoh <japanjetone@live.jp>
2017-12-12 17:57:03 +0900

    codeLib1から移植

---
+++ src/cpp/random/datastruct/rand_chooser.cpp
@@ -0,0 +1,51 @@
+// %title
+// 禁止可能な一様分布の乱数
+// 
+// %overview
+// 一部の整数を取り出せないようにできるような，一様分布の乱数を生成する．
+// invalidな引数を与えた場合の動作は未定義．
+// 空間計算量 O(size) に注意する．
+//
+// %usage
+// RandChooser(size_t size)
+// ; [0, size-1] の範囲の整数の乱数を生成するようなインスタンスの生成
+// inline int RandChooser::operator()(RANDOM &rd)
+// rd : 乱数生成器．
+// ; 乱数生成する．
+// inline int RandChooser::disable(int idx)
+// ; 整数idxを次回以降の乱数生成で選ばれないように設定する．
+// ; 既にdisableした値をもう一度disableする操作はinvalid．
+//
+// %verified
+// 
+// %references
+// 
+
+
+class RandChooser{
+    size_t size;
+    size_t left_size;
+    vector<int> swapper;
+    vector<int> unswapper;
+public:
+    RandChooser(size_t s):size(s),left_size(s),swapper(s),unswapper(s){
+        for (int i=0; i < s; ++i){
+            swapper[i] = unswapper[i] = i;
+        }
+    }
+    inline size_t left(){
+        return left_size;
+    }
+    
+    template<typename RANDOM>
+    inline int operator()(RANDOM &rd){
+        uniform_int_distribution<int> rand_int(0, left_size-1);
+        return swapper[rand_int(rd)];
+    }
+    
+    inline int disable(int idx){
+        swap(swapper[unswapper[idx]],swapper[left_size-1]);
+        unswapper[swapper[unswapper[idx]]] = unswapper[idx];
+        return --left_size;
+    }
+};
---
+++ src/cpp/random/method/rand_multi.cpp
@@ -0,0 +1,45 @@
+// %title
+// 1からnまでの乱数をm個重複せずランダムに選ぶ
+// 
+// %overview
+// 1からnまでの乱数をm個重複せずランダムに選ぶ．
+// このアルゴリズムは空間計算量O(m)，時間計算量O(m)で済む（ハッシュ操作をO(1)とする）．
+// 簡単な実装方法として
+// - 重複しないm個を取り出すまで乱数を引く (n>>m なら衝突しにくいので高速)
+// - iotaしてshuffleしてeraseする（空間O(n)，時間O(n)）
+// があるが，計算量的には両者のどちらよりも優れていることが分かる．
+// m/nの比でアルゴリズムを選択するハイブリッド実装でも良さそうである．
+//
+// [TODO] rand_int の除去
+//
+// %usage
+// void pick_multirand(int n, int m, vector<int>& out)
+// ; 1からnまでの乱数をm個重複せずランダムに選びoutにpush_backする
+// inline int RandChooser::operator()(RANDOM &rd)
+// rd : 乱数生成器．
+// ; 乱数生成する．
+// inline int RandChooser::disable(int idx)
+// ; 整数idxを次回以降の乱数生成で選ばれないように設定する．
+// ; 既にdisableした値をもう一度disableする操作はinvalid．
+//
+// %verified
+// 
+// %references
+// 
+
+// 
+void pick_multirand(int n, int m, vector<int>& out){
+    out.clear();
+    unordered_map<int, int> memo;
+    if (n < m) m = n;
+    for (int low = 1; low <= m; ++low){
+        int r = rand_int(low, n);
+        if (!memo[r]){
+            out.emplace_back(r);
+        }else{
+            out.emplace_back(memo[r]);
+        }
+        int e = memo[low];
+        memo[r] = e ? e : low; 
+    }
+}
