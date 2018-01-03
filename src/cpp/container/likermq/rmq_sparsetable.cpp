92c5363bafec8eaed8f1d4bf8f0071058c50a594
buyoh <japanjetone@live.jp>
2018-01-04 01:34:27 +0900

    sparse table

---
+++ src/cpp/container/likermq/rmq_sparsetable.cpp
@@ -0,0 +1,74 @@
+// %title
+// SparseTable(区間最小)
+// 
+// %overview
+// 構築済みの配列に対して，次のクエリを処理できる．
+// - 区間の最小値を計算する．
+// 0-indexedで，[begin,end)．beginを含み，endを含まない．
+// 何度も加減算を繰り返すと内部でオーバーフローを起こす可能性．
+// インデックスは size_t ではなく int で扱う．[TODO]
+// O(log^2N)．最大値のみの機能なら出来そう．[TODO]
+//
+// %usage
+// SparseTable<typename T>(int n)
+// ; [0,n) の配列を確保する．
+// T& SparseTable::operator[](size_t i)
+// ; i の要素の参照を得る．build後に更新してはならない．
+// void SparseTable::build()
+// ; クエリに応えられるように準備する．
+// void SegmentTree::add_valrange(int begin, int end, T e)
+// ; 区間[begin,end)にeを加算する
+// void SegmentTree::get_maxrange(int idx, T e)
+// ; 区間[begin,end)の最大値を計算する
+// void SegmentTree::get_maxrangeIdx(int idx, T e)
+// ; 区間[begin,end)の最大値が存在する要素のインデックスを取得する
+//
+// %verified
+// http://yukicoder.me/submissions/172470
+// 
+// %references
+// http://tookunn.hatenablog.com/entry/2016/07/13/211148
+
+template <typename T>
+class SparseTable {
+public:
+    int size;
+    vector<int> log2;
+    vector<T> data;
+    vector<T> dp;
+
+    SparseTable(int size) :size(size), log2(size + 1), data(size) {
+        // for fast calculate log2
+        for (int i = 2; i <= size; ++i) {
+            log2[i] = log2[i >> 1] + 1;
+        }
+
+        dp.resize(size*(log2[size] + 1));
+    }
+
+    inline T& operator[](size_t i) { return data[i]; }
+    inline T operator[](size_t i)const { return data[i]; }
+
+    void build() {
+        int l, i, f, b;
+        for (i = 0; i < size; i++) {
+            dp[i] = i;
+        }
+        for (l = 1; (1 << l) <= size; l++) {
+            for (i = 0; i + (1 << l) <= size; i++) {
+                f = dp[i + size * (l - 1)];
+                b = dp[(i + (1 << (l - 1))) + size * (l - 1)];
+
+                dp[i + size * l] = (data[f] <= data[b]) ? f : b; // minimum
+            }
+        }
+    }
+
+    // range [l,r)
+    int getminrangeIdx(int l, int r) const {
+        int lg = log2[r - l];
+        int i1 = dp[l + size * lg];
+        int i2 = dp[r - (1 << lg) + size * lg];
+        return (data[i1] <= data[i2]) ? i1 : i2; // minimum
+    }
+};
