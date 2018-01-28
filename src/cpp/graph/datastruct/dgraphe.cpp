5bed5bf4107ad4157c45b9b55844f7e935316f1d
buyoh <japanjetone@live.jp>
2018-01-28 23:10:56 +0900

    bugfix

--- src/cpp/graph/datastruct/dgraphe.cpp
+++ src/cpp/graph/datastruct/dgraphe.cpp
@@ -24,7 +24,7 @@ public:
     struct Arc {
         int from, to;
         W_T value;
-        Arc(int f = 0, int t = 0, W_T value = 0) :from(f), to(to), value(value) {}
+        Arc(int f = 0, int t = 0, W_T value = 0) :from(f), to(t), value(value) {}
         inline int pair(int _v) const { return _v == to ? from : to; }
     };
     size_t n;
@@ -32,18 +32,19 @@ public:
     vector<vector<int>> vertex_from;
     vector<Arc> arcs;
 
-    DGraphE(int n = 1) :n(n), vertex_to(n), vertex_from(n){ }
+    DGraphE(int n = 1) :n(n), vertex_to(n), vertex_from(n) { }
 
     void resize(size_t _n) {
         n = _n;
         vertex_to.resize(_n);
+        vertex_from.resize(_n);
     }
     void connect(int from, int to, W_T val = 0) {
         vertex_to[(size_t)from].push_back((int)arcs
-.size());
+            .size());
         vertex_from[(size_t)to].push_back((int)arcs
-.size());
+            .size());
         arcs
-.emplace_back(from, to, val);
+            .emplace_back(from, to, val);
     }
 };
