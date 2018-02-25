cb1f7f72ec1ed394a83ebfd2e3aa9f8ffc6f1cb6
buyoh <japanjetone@live.jp>
2018-02-25 13:35:39 +0900

    決定木(仮)

---
+++ src/cpp/predicator/decisiontree.cpp
@@ -0,0 +1,35 @@
+
+
+template<typename input_t, typename output_t>
+class DecisionTree {
+    using comparator_t = function<bool(input_t)>;
+
+    bool leaf_;
+    comparator_t comparator_;
+    output_t out_;
+    unique_ptr<DecisionTree> childlen_[2];
+public:
+
+    DecisionTree() :leaf_(true), comparator_(nullptr) {}
+
+    inline DecisionTree& operator[](int i) { return *childlen_[i]; }
+    inline bool leaf() { return leaf_; }
+
+    inline void generate_leaf(output_t out) {
+        leaf_ = true;
+        childlen_[0].release();
+        childlen_[1].release();
+        out_ = out;
+    }
+
+    inline void generate_branch(comparator_t comparator) {
+        leaf_ = false;
+        childlen_[0].reset(new DecisionTree());
+        childlen_[1].reset(new DecisionTree());
+        comparator_ = comparator;
+    }
+
+    output_t eval(input_t val) {
+        return leaf() ? out_ : childlen_[comparator_(val)]->eval(val);
+    }
+};
