781cb17c162c7f9aea67b9fc4ff443aa115b4985
buyoh <japanjetone@live.jp>
2018-03-30 15:54:43 +0900

    永続

---
+++ src/cpp/container/persistent/parray.cpp
@@ -0,0 +1,77 @@
+// %title
+// 永続配列
+// 
+// %overview
+// 通常の固定長配列の機能に加えて，undoを持つ．
+// undoした操作をundo出来る．
+//
+// %usage
+// void undo(int count)
+// ; count回数もとに戻る
+// vector<value_t> get()
+// ; 現在の配列を取得する
+//
+// %verified
+// [TODO]
+// 
+// %references
+// 
+// %words
+// persistent,stack,eizoku
+// %require
+// 
+
+struct PersistentArray {
+    using value_t = int;
+    struct Node {
+        enum Type {
+            UPDATE, NONE
+        };
+        Type type;
+        int index;
+        value_t val;
+        int next_idx;
+        Node(Type t = Type::NONE, int i = 0, value_t v = 0)  :type(t), index(i), val(v) { }
+    };
+
+    int n;
+    vector<Node> history;
+
+    PersistentArray(int _size):n(_size) {
+        history.emplace_back(Node::Type::NONE);
+    }
+
+    inline void update(int index, value_t val) {
+        Node node = Node(Node::Type::UPDATE, val);
+        node.next_idx = history.size() - 1;
+        history.push_back(node);
+    }
+
+    inline void undo(int count) {
+        int n = history.size();
+        assert(0 <= (n - 1 - count));
+        history.push_back(history[n - 1 - count]);
+    }
+
+    inline vector<value_t> get() {
+        int it = history.size() - 1;
+        
+        vector<int> wrote(n);
+        vector<value_t> res(n);
+
+        while (history[it].type != Node::Type::NONE) {
+            const Node& node = history[it];
+            if (node.type == Node::Type::UPDATE) {
+                if (!wrote[node.index]) {
+                    wrote[node.index] = true;
+                    res[node.index] = node.val;
+                }
+            }
+            it = history[it].next_idx;
+        }
+
+        return res;
+    }
+};
+
+
---
+++ src/cpp/container/persistent/pstack.cpp
@@ -0,0 +1,79 @@
+// %title
+// 永続スタック
+// 
+// %overview
+// 通常のスタックの機能に加えて，undoを持つ．
+// undoした操作をundo出来る．
+//
+// %usage
+// void undo(int count)
+// ; count回数もとに戻る
+// vector<value_t> get()
+// ; 現在のスタックの状態を取得する
+//
+// %verified
+// 乱数
+// 
+// %references
+// https://www.slideshare.net/qnighy/re-15443018
+// %words
+// persistent,stack,eizoku
+// %require
+// 
+
+struct PersistentStack {
+    using value_t = int;
+    struct Node {
+        enum Type {
+            PUSH, POP, NONE
+        };
+        Type type;
+        value_t val;
+        int next_idx;
+        Node(Type t = PUSH, value_t v = 0)  :type(t), val(v) { }
+    };
+
+    vector<Node> history;
+
+    PersistentStack() {
+        history.emplace_back(Node::Type::NONE);
+    }
+
+    inline void push(value_t val) {
+        Node node = Node(Node::Type::PUSH, val);
+        node.next_idx = history.size() - 1;
+        history.push_back(node);
+    }
+    inline void pop() {
+        Node node = Node(Node::Type::POP);
+        node.next_idx = history.size() - 1;
+        history.push_back(node);
+    }
+
+    inline void undo(int count) {
+        int n = history.size();
+        assert(0 <= (n - 1 - count));
+        history.push_back(history[n - 1 - count]);
+    }
+
+    inline vector<value_t> get() {
+        Node* p = &(history.back());
+        
+        vector<value_t> res;
+        int popcount = 0;
+        while (p->type != Node::Type::NONE) {
+            if (p->type == Node::Type::PUSH) {
+                if (popcount > 0)
+                    --popcount;
+                else
+                    res.push_back(p->val);
+            }
+            else if (p->type == Node::Type::POP) {
+                ++popcount;
+            }
+            p = &(history[p->next_idx]);
+        }
+        reverse(ALL(res));
+        return res;
+    }
+};
