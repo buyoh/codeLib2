d8d6d11bb4e819b6248bb8ba893299bce7e1ba22
buyoh <japanjetone@live.jp>
2018-07-02 00:36:34 +0900

    高速化

--- src/cpp/math/method/math_utils.cpp
+++ src/cpp/math/method/math_utils.cpp
@@ -30,11 +30,9 @@ template<typename T> inline T lcm(T a, T b) { return a*b/gcd(a,b); }
 // MEMO : 逆元...pow_md(n,MOD-2,MOD)
 template<typename T> T pow_md(T x, T p, T mod = MD) {
     T y = 1; x = x%mod;
-    while (0<p) {
-        if (p & 1)
-            y = (y*x) % mod;
-        x = (x*x) % mod;
-        p >>= 1;
+    for (;0 < p; p >>= 1) {
+        if (p & 1) y = y*x % mod;
+        x = x*x % mod;
     }
     return y;
 }
@@ -45,10 +43,12 @@ template<typename T> T nPr_md(T n, T r, T mod = MD) {
     T p = 1; for (r = n - r; r<n; p = (p*n--) % mod); return p;
 }
 template<typename T> T nCr_md(T n, T r, T mod = MD) {
-    if (n / 2 < r) return nCr_md(n, n - r);
-    T p = 1; for (; 0<r; r--)
-        p = (((p*(n - r + 1)) % mod)*pow_md(r, mod - 2)) % mod;
-    return p;
+    if (n < 0 || r < 0 || n < r) return 0;
+    if (n / 2 < r) return nCr_md(n, n - r, mod);
+    T num = 1, den = 1;
+    for (; 0 < r; --r, --n)
+        num = num*n % mod, den = den*r % mod;
+    return num * pow_md(den, mod - 2, mod) % mod;
 }
 template<typename T> inline T nHr_md(T n, T r, T mod = MD) {
     return nCr_md(n - 1 + r, n - 1, mod);
