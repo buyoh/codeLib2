72f812692e3f2528c960a583d2b6e89ca2d22b8c
buyoh <japanjetone@live.jp>
2018-07-28 00:09:59 +0900

    prime(constexpr)

--- src/cpp/math/method/prime/primesieve.cpp
+++ src/cpp/math/method/prime/primesieve.cpp
@@ -3,13 +3,15 @@
 // 
 // %overview
 // エラトステネスの篩
+// constexpr版もある(IsPrimeC)W
 //
 // %usage
-// void gen_primelist(vector<int>& out, ll high);
-// out  : 
-// high : 
-// `out[i]==1`ならばiは合成数
-// `out.resize(high+1)` する．
+// vector<int> gen_primelist(ll high);
+// ; high : 
+// ; `out[i]==1`ならばiは!!合成数!!
+// 
+// constexpr IsPrimeC<2000> ipc;
+// ; `ipc[x] == true` なら，xは!!素数!!
 //
 // %verified
 // 
@@ -19,15 +21,48 @@
 
 
 
-void gen_primelist(vector<int>& out, ll high) {
+vector<int> gen_primelist(ll high) {
+    vector<int> out;
     out.resize(high + 1);
     out[0] = out[1] = 1;
     ll sqh = (ll)sqrt(high);
     for (ll i = 2; i <= sqh; i++) {
         if (out[i] == 0) {
-            for (ll j = i*i; j <= high; j += i) { // i*i
+            for (ll j = i * i; j <= high; j += i) { // i*i
                 out[j] = 1;
             }
         }
     }
+    return out;
 }
+
+
+template<int Max = 2000>
+class IsPrimeC {
+    int d_[Max + 1];
+
+    template<typename T>
+    constexpr T cesqrt(T s) {
+        double x = s / 2.0;
+        double prev = 0.0;
+        while (x != prev) {
+            prev = x;
+            x = (x + s / x) / 2.0;
+        }
+        return x;
+    }
+public:
+    constexpr IsPrimeC(): d_() {
+        d_[0] = d_[1] = 1;
+        int sqh = cesqrt<int>(Max);
+        for (int i = 2; i <= sqh; i++) {
+            if (d_[i] == 0) {
+                for (int j = i * i; j <= Max; j += i) { // i*i
+                    d_[j] = 1;
+                }
+            }
+        }
+        for (int i = 0; i <= Max; ++i) d_[i] = !d_[i];
+    }
+    constexpr inline bool operator[](int x) const{return d_[x]; }
+};
