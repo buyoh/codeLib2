e0cc9872fcb8f17aeabc6df4413aa01589b14871
buyoh <japanjetone@live.jp>
2018-08-29 11:36:01 +0900

    近代化改修
    initializer_list，vectorとの掛け算のテンプレート化，演算子オーバーロードの不備修正，行数を減らした

--- src/cpp/math/datastruct/matrix.cpp
+++ src/cpp/math/datastruct/matrix.cpp
@@ -5,22 +5,30 @@
 // 行列同士の+-*，スカラーとの+-*/，べき乗を実装
 //
 // %usage
-// [TODO]
+// ;定数配列を指定する時は，
+// Matrix<ll> mat(3, 3, {
+//    1, 2, 3, 
+//    3, 1, 2,
+//    2, 3, 1
+// });
 //
 // %verified
-// 
+// https://beta.atcoder.jp/contests/arc027/submissions/3095831
+// など
 // %references
 
 
 
 template<typename T>
-// typedef double T;
+// using T = double;
 class Matrix {
 public:
     size_t height_, width_;
     valarray<T> data_;
-    Matrix(size_t height, size_t width) :height_(height), width_(width), data_(height*width) {}
-    Matrix(size_t height, size_t width, const valarray<T>& data) :height_(height), width_(width), data_(data) {}
+    Matrix(size_t height = 1, size_t width = 1) :height_(height), width_(width), data_(height*width) {}
+    template <typename V>
+    Matrix(size_t height, size_t width, const V& data) : height_(height), width_(width), data_(data) {}
+    Matrix(size_t height, size_t width, initializer_list<T> init) : height_(height), width_(width), data_(init) {}
 
     inline T& operator()(size_t y, size_t x) { return data_[y*width_ + x]; }
     inline T operator() (size_t y, size_t x) const { return data_[y*width_ + x]; }
@@ -39,76 +47,46 @@ public:
             }os << endl;
         }
     }
-    valarray<valarray<T>> to_valarray() const {
-        valarray<valarray<T>> work(height_);
-        for (size_t i = 0; i < height_; ++i) {
-            auto &v = work[i]; v.resize(height_);
-            for (size_t j = 0; j < width_; ++j)
-                v[j] = at(i, j);
-        } return work;
-    }
-    // mathematics
-    Matrix<T> pow(long long);
-    double det() const; T tr();
-    Matrix<T>& transpose_self(); Matrix<T> transpose() const;
-    struct LU {
-        size_t size;
-        vector<int> pivot;
-        vector<T> elem;
-    };
 };
-
-// IO
 template<typename T> inline ostream& operator << (ostream& os, Matrix<T> mat) { mat.print(os); return os; }
 
-// 掛け算
 template<typename T> Matrix<T> multiply(const Matrix<T>& mat1, const Matrix<T>& mat2) {
     assert(mat1.width_ == mat2.height_);
     Matrix<T> result(mat1.height_, mat2.width_);
-    for (size_t i = 0; i < mat1.height_; i++) {
-        for (size_t j = 0; j < mat2.width_; j++) {
-            for (size_t k = 0; k < mat1.width_; k++) {
+    for (size_t i = 0; i < mat1.height_; ++i)
+        for (size_t j = 0; j < mat2.width_; ++j)
+            for (size_t k = 0; k < mat1.width_; ++k)
                 result(i, j) += mat1(i, k) * mat2(k, j);
-            }
-        }
-    }
     return result;
 }
-template<typename T> valarray<T> multiply(const Matrix<T>& mat1, const valarray<T>& vec2) {
+template<typename T, typename V> V multiply(const Matrix<T>& mat1, const V& vec2) {
     assert(mat1.width_ == vec2.size());
-    valarray<T> result(mat1.height_);
-    for (size_t i = 0, j; i < mat1.height_; i++) {
-        for (j = 0; j < mat1.width_; j++) {
+    V result(mat1.height_);
+    for (size_t i = 0, j; i < mat1.height_; ++i)
+        for (j = 0; j < mat1.width_; ++j)
             result[i] += mat1(i, j) * vec2[j];
-        }
-    }
     return result;
 }
-template<typename T> inline Matrix<T>& operator*=(Matrix<T>& mat1, Matrix<T>& mat2) { mat1 = multiply(mat1, mat2); return mat1; }
-template<typename T> inline Matrix<T> operator*(Matrix<T>& mat1, Matrix<T>& mat2) { return multiply(mat1, mat2); }
 
-
-// スカラー
 template<typename T> inline Matrix<T>& operator+=(Matrix<T>& mat, T val) { mat.data_ += val; return mat; }
+template<typename T> inline Matrix<T>& operator-=(Matrix<T>& mat, T val) { mat.data_ -= val; return mat; }
 template<typename T> inline Matrix<T>& operator*=(Matrix<T>& mat, T val) { mat.data_ *= val; return mat; }
 template<typename T> inline Matrix<T>& operator/=(Matrix<T>& mat, T val) { mat.data_ /= val; return mat; }
 template<typename T> inline Matrix<T>& operator^=(Matrix<T>& mat, T val) { mat.data_ ^= val; return mat; }
+template<typename T> inline Matrix<T>& operator+=(Matrix<T>& mat1, const Matrix<T>& mat2) { mat1.data_ += mat2.data_; return mat1; }
+template<typename T> inline Matrix<T> operator+(Matrix<T>& mat1, const Matrix<T>& mat2) { return Matrix<T>(mat1.height_, mat1.width_, mat1.data_ + mat2.data_); }
+template<typename T> inline Matrix<T>& operator-=(Matrix<T>& mat1, const Matrix<T>& mat2) { mat1.data_ -= mat2.data_; return mat1; }
+template<typename T> inline Matrix<T> operator-(Matrix<T>& mat1, const Matrix<T>& mat2) { return Matrix<T>(mat1.height_, mat1.width_, mat1.data_ - mat2.data_); }
+template<typename T> inline Matrix<T>& operator*=(Matrix<T>& mat1, const Matrix<T>& mat2) { mat1 = multiply(mat1, mat2); return mat1; }
+template<typename T> inline Matrix<T> operator*(const Matrix<T>& mat1, const Matrix<T>& mat2) { return multiply(mat1, mat2); }
+template<typename T, typename V> inline V operator*(const Matrix<T>& mat1, const V& vec2) { return multiply(mat1, vec2); }
 
-// 行列
-template<typename T> inline Matrix<T>& operator+=(Matrix<T>& mat1, Matrix<T>& mat2) { mat1.data_ += mat2.data_; return mat1; }
-template<typename T> inline Matrix<T> operator+(Matrix<T>& mat1, Matrix<T>& mat2) { return Matrix<T>(mat1.height_, mat1.width_, mat1.data_ + mat2.data_); }
-
-
-template<typename T> Matrix<T> Matrix<T>::pow(long long p) {
-    assert(height_ == width_);
-    Matrix<T> a = *this;
-    Matrix<T> b(height_, height_); b.setDiag(1);
-
+template<typename T> Matrix<T> pow(Matrix<T> a, long long p) {
+    assert(a.height_ == a.width_);
+    Matrix<T> b(a.height_, a.height_); b.setDiag(1);
     while (0 < p) {
-        if (p % 2) {
-            b *= a;
-        }
-        a *= a; p /= 2;
+        if (p & 1) b *= a;
+        a *= a; p >>= 1;
     }
     return b;
 }
