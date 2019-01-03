a7120ad84c4d0e0fdce2949fcaca4c0c081d91a7
buyoh <japanjetone@live.jp>
2019-01-03 23:54:48 +0900

    shuffle, combiperm

---
+++ src/javascript/extends/array/combperm.js
@@ -0,0 +1,59 @@
+// %title
+// 組合せ・順列列挙
+// 
+// %overview
+// ジェネレータを返す．ジェネレータは，組合せ・順列を列挙する．
+// ruby の Array#permutation，Array#combination に似た挙動．
+// 高等なアルゴリズムは使わず，雑な再帰で実装．
+//
+// %usage
+// for (let e of [1,2,3,4].combination(2)) console.log(e);
+//
+// %verified
+// 
+// %references
+// https://qiita.com/komaji504/items/62a0f8ea43053e90555a
+// %words
+// shuffle
+// %require
+// 
+
+Array.prototype.permutation = function*() {
+    if (this.length <= 0) return;
+    const n = this.length;
+    const this_ = this;
+    function* dfs(p) {
+        if (p.length == n){
+            yield p.map(i => this_[i]);
+            return ;
+        }
+        for (let i = 0; i < n; ++i){
+            if (p.some(e => e==i)) continue;
+            p.push(i);
+            for (let r of dfs(p)) yield r;
+            p.pop();
+        }
+    }
+    for (let r of dfs([])) yield r;
+}
+
+Array.prototype.combination = function*(k) {
+    if (this.length <= 0 || (this.length < k)) return;
+    const n = this.length;
+    const this_ = this;
+    function* dfs(c, idx) {
+        if (n - idx < k - c.length) return;
+        if (idx == n){
+            if (c.length == k)
+                yield c.map(i => this_[i]);
+            return ;
+        }
+        if (k > c.length) {
+            c.push(idx);
+            for (let r of dfs(c, idx+1)) yield r;
+            c.pop();
+        }
+        for (let r of dfs(c, idx+1)) yield r;
+    }
+    for (let r of dfs([], 0)) yield r;
+}
---
+++ src/javascript/extends/array/shuffle.js
@@ -0,0 +1,29 @@
+// %title
+// shuffle (Fisher–Yates)
+// 
+// %overview
+// 配列をshuffleする
+// 配列はコピーされず書き換えられる
+//
+// %usage
+// [1,2,3].shuffle()
+//
+// %verified
+// 
+// %references
+// https://qiita.com/komaji504/items/62a0f8ea43053e90555a
+// %words
+// shuffle
+// %require
+// 
+
+
+Array.prototype.shuffle = function() {
+    for(let i = this.length - 1; i > 0; --i){
+        let r = Math.floor(Math.random() * (i + 1));
+        let t = this[i];
+        this[i] = this[r];
+        this[r] = t;
+    }
+    return this;
+};
