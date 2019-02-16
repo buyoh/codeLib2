7ffcdc6c781e1c982c4f18fb1d3e1b909152ba1f
buyoh <japanjetone@live.jp>
2019-02-16 14:33:59 +0900

    dijkstra

---
+++ src/cpp/graph/method/dijkstra/dijkstraWE.cpp
@@ -0,0 +1,43 @@
+// %title
+// ダイクストラ法(無向・辺重み)
+// 
+// %overview
+//
+// %usage
+//
+// %words
+// dijkstra
+// %require
+// cpp/graph/datastruct/graphe.cpp
+// %verified
+// 
+// %references
+// 
+
+
+
+
+vector<GraphE::W_T> dijkstraEdgeWeighted(const GraphE& graph, int startIndex) {
+    using T = GraphE::W_T;
+    vector<T> dist(graph.size(), numeric_limits<T>::max());
+
+    priority_queue<pair<T, int>> que; // <dist, idx>
+    que.emplace(0, startIndex);
+    dist[startIndex] = 0;
+
+    while (!que.empty()) {
+        int d = -que.top().first;
+        int v = que.top().second;
+        que.pop();
+        if (dist[v] < d) continue;
+
+        for (int ei : graph.vertex_to[v]) {
+            auto e = graph.edges[ei];
+            int u = e.to(v);
+            if (d + e.value < dist[u]) {
+                que.emplace(-(dist[u] = d + e.value), u);
+            }
+        }
+    }
+    return move(dist);
+}
---
+++ src/cpp/graph/method/dijkstra/dijkstraWV.cpp
@@ -0,0 +1,38 @@
+// %title
+// ダイクストラ法(無向・頂点重み)
+// 
+// %overview
+//
+// %usage
+//
+// %words
+// dijkstra
+// %require
+// cpp/graph/datastruct/graph.cpp
+// %verified
+// 
+// %references
+// 
+
+
+template<typename T, typename ITER>
+vector<T> dijkstraVertexWeighted(const Graph& graph, int startIndex, const ITER& weight) {
+    vector<T> dist(graph.size(), numeric_limits<int>::max());
+
+    priority_queue<pair<T, int>> que; // <dist, idx>
+    que.emplace(0, startIndex);
+    dist[startIndex] = 0;
+
+    while (!que.empty()) {
+        int d = -que.top().first;
+        int v = que.top().second;
+        que.pop();
+        if (dist[v] < d) continue;
+
+        for (int u : graph.vertex_to[v])
+            if (d + weight[u] < dist[u])
+                que.emplace(-(dist[u] = d + weight[u]), u);
+    }
+    return move(dist);
+}
+
