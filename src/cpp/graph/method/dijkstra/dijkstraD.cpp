f7744998e301b7f902669891a1cf48f9712c2c09
舞葉 <japanjetone@live.jp>
2019-06-29 23:42:35 +0900

    add directed dijkstra

---
+++ src/cpp/graph/method/dijkstra/dijkstraD.cpp
@@ -0,0 +1,35 @@
+// %title
+// ダイクストラ法(有向・重み無し)
+// 
+// %overview
+//
+// %usage
+//
+// %words
+// dijkstra
+// %require
+// cpp/graph/datastructure/graph.cpp
+// %verified
+// https://atcoder.jp/contests/abc132/submissions/6175759
+// %references
+// 
+
+vector<int> dijkstraD(const DGraph& graph, int startIndex) {
+    vector<int> dist(graph.size(), numeric_limits<int>::max());
+ 
+    queue<pair<int, int>> que; // <dist, idx>
+    que.emplace(0, startIndex);
+    dist[startIndex] = 0;
+ 
+    while (!que.empty()) {
+        int d = que.front().first;
+        int v = que.front().second;
+        que.pop();
+        if (dist[v] < d) continue;
+ 
+        for (int u : graph.vertex_to[v])
+            if (d + 1 < dist[u])
+                que.emplace(dist[u] = d + 1, u);
+    }
+    return move(dist);
+}
