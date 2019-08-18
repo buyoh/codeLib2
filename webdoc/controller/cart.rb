b678d9ff22b77fbf7eb962bf1267336f9aad47f3
buyoh <japanjetone@live.jp>
2019-08-19 00:46:19 +0900

    refactor cart

--- webdoc/controller/cart.rb
+++ webdoc/controller/cart.rb
@@ -1,4 +1,24 @@
 
+def init_cart()
+  set_cookie('cart', '{"i":[]}', 3600*3)
+  return {'i'=>[]}
+end
+
+def get_cart()
+  cart = get_cookie('cart')
+  cart = init_cart() if cart.nil?
+  cart
+end
+
+def set_cart(cart)
+  set_cookie('cart', cart)
+end
+
+def include_cart(cart, id)
+  cart['i'].include?(id)
+end
+
+
 get '/cart' do
   @docs = []
   err = []
@@ -21,6 +41,11 @@ get '/cart' do
   @gen_code = DBUtil.generate_merged_code(solved[:path_sequence], solved[:docs])
 
   erb :cart
+end
+
+
+before do
+  @cart = get_cart()
 end
 
 
--- webdoc/server.rb
+++ webdoc/server.rb
@@ -33,30 +33,6 @@ def get_cookie(name)
   h
 end
 
-
-# - - - - - - - - - - - - - - - - - - - - - - - 
-
-
-def init_cart()
-  set_cookie('cart', '{"i":[]}', 3600*3)
-  return {'i'=>[]}
-end
-
-def get_cart()
-  cart = get_cookie('cart')
-  cart = init_cart() if cart.nil?
-  cart
-end
-
-def set_cart(cart)
-  set_cookie('cart', cart)
-end
-
-def include_cart(cart, id)
-  cart['i'].include?(id)
-end
-
-
 # - - - - - - - - - - - - - - - - - - - - - - - 
 
 
@@ -69,7 +45,7 @@ end
 
 
 before do
-  @cart = get_cart()
+  
 end
 
 
