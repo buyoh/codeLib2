1a04b80b23e2585e0e121875bc855ae2b5d7d90b
buyoh <japanjetone@live.jp>
2019-10-15 02:00:09 +0900

    implement around the database

--- tools/dbhelper/dbhelper.rb
+++ tools/dbhelper/dbhelper.rb
@@ -1,4 +1,6 @@
 require "#{__dir__}/dbwrapper"
+require "#{__dir__}/collector"
+require "#{__dir__}/gitlog"
 
 class DBHelper
 
@@ -16,27 +18,44 @@ class DBHelper
   def update_db()
 
     docs = nil
+    lastcommits = nil
     Dir.chdir('../') do
       docs = Document.collect_documents()
+      lastcommits = GitLog.collect_all_latest_nocache(docs.map{|doc| doc[:path]})
     end
-
     index = all_index_db()
+    shas = gather_sha()
 
     docs.each do |doc|
       doc[:words] = '' unless doc[:words]
 
-      # existed = @sqldb.execute("select id from articles where path=?;", doc[:path])
-      
-      existed = index.select{|article| article[:path] == doc[:path]}
+      commit = lastcommits[doc[:path]] || {sha: '#invalid', date: Time.local(2000), message: ''}
+      sha_exists = shas[commit[:sha]]
+
+      commitId = nil
+      if sha_exists
+        commitId = sha_exists[:id]
+      else
+        sha = commit[:sha]
+        @dbwrapper.insert_gitcommit(sha, commit[:date].to_i, commit[:message])
+        commitId = @dbwrapper.get_gitcommit_sha(sha)[0]
+        shas[sha] = {id: commitId, date: commit[:date].to_i, message: commit[:message]}
+      end
+
+      article_exists = index.select{|article| article[:path] == doc[:path]}
 
       id = nil
-      if existed.empty?
-        @dbwrapper.insert_article(doc[:path], doc[:title], doc[:words])
+      if article_exists.empty?
+        @dbwrapper.insert_article(doc[:path], doc[:title], doc[:words], commitId)
         id = @dbwrapper.getid_article(doc[:path])
+        index << {id: id, path: doc[:path], title: doc[:title], words: doc[:words], checked: true}
       else
-        existed[0][:checked] = true
-        id = existed[0][:id]
-        @dbwrapper.update_article(id, doc[:path], doc[:title], doc[:words])
+        article_exists[0][:checked] = true
+        a = article_exists[0]
+        id = a[:id]
+        if a[:commitId] != commitId || a[:title] != doc[:title] || a[:words] != a[:words]
+          @dbwrapper.update_article(id, doc[:path], doc[:title], doc[:words], commitId)
+        end
       end
 
       doc.each do |key, val|
@@ -58,17 +77,29 @@ class DBHelper
   end
 
 
-  # すべての {id, path, title, words} を配列に返す
+  # すべての {id, path, title, words, commitId} を配列に返す
   def all_index_db()
     @dbwrapper.all_articles() \
-      .map{|id,path,title,keyword| {id:id, path:path, title:title, words:keyword} }
+      .map{|id,path,title,words,commitId| {id: id, path: path, title: title, words: words, commitId: commitId} }
+  end
+
+  # すべての {id, path, title, words, date, sha} を配列に返す
+  def all_index_db_solved()
+    @dbwrapper.all_articles_joined_commit() \
+      .map{|id,path,title,words,date,sha| {id: id, path: path, title: title, words: words, date: Time.at(date), sha: sha} }
   end
 
 
-  # 検索に一致した {id, path, title, words} の配列を返す
-  def search_index_db(path, title, keyword, union="or")
-    @dbwrapper.search_articles(path, title, keyword, union) \
-      .map{|id,path,title,keyword| {id:id, path:path, title:title, words:keyword} }
+  # 検索に一致した {id, path, title, words, commitId} の配列を返す
+  def search_index_db(path, title, words, union="or")
+    @dbwrapper.search_articles(path, title, words, union) \
+      .map{|id,path,title,words,commitId| {id: id, path: path, title: title, words: words, commitId: commitId} }
+  end
+
+  # commitId のコミット情報をdbから取得する
+  def get_gitcommit(id)
+    id, sha, date, message = @dbwrapper.get_gitcommit(id)
+    {id: id, sha: sha, date: Time.at(date), message: message}
   end
 
 
@@ -83,12 +114,12 @@ class DBHelper
   end
 
 
-  # idの値から {id, path, title, words} を求める
+  # idの値から {id, path, title, words, commitId, ...details} を求める
   def find_db_by_index(id)
     r = @dbwrapper.get_article(id)
     return nil unless r
-    path, title, keyword = r
-    { id: id, path: path, title: title, words: keyword }.update(self.get_detail(id))
+    path, title, words, commitId = r
+    { id: id, path: path, title: title, words: words, commitId: commitId }.update(self.get_detail(id))
   end
 
 
@@ -96,9 +127,19 @@ class DBHelper
   def find_db_by_path(path)
     r = @dbwrapper.get_article_path(path)
     return nil unless r
-    id, path, title, keyword = r
+    id, path, title, keyword, commitId = r
     id = id.to_i
-    { id: id, path: path, title: title, words: keyword }.update(self.get_detail(id))
+    { id: id, path: path, title: title, words: keyword, commitId: commitId }.update(self.get_detail(id))
+  end
+
+
+  # 
+  def gather_sha()
+    h = {}
+    @dbwrapper.all_gitcommits.each do |id, sha, date, message|
+      h[sha] = {id: id, date: Time.at(date), message: message}
+    end
+    h
   end
 
 
--- tools/dbhelper/gitlog.rb
+++ tools/dbhelper/gitlog.rb
@@ -1,17 +1,19 @@
+require 'time'
 require "#{__dir__}/collector.rb"
 
 module GitLog
 
-  def self.collect_all_nocache(n = 4)
-    # 深さを指定してコミットとファイルを取得
-    files = Document.src_files
+  # 事前知識なしでfilesのすべてのlatest commitの情報を取得する。
+  # files: Document.src_files
+  # @result: filepath => {sha, date, message}
+  def self.collect_all_latest_nocache(files, per_skip = 4)
     db = files.reduce(Hash.new){|s,f| s[f] = nil; s}
     remain = files.size
 
     skip = 0
     while remain > 0
       none = true
-      IO.popen("git log -n #{n} --skip=#{skip} --name-only --oneline --pretty=format:'%h;%cd;%s' --date=short", 'r') do |io|
+      IO.popen("git log -n #{per_skip} --skip=#{skip} --name-only --oneline --pretty=format:'%H;%cd;%s'", 'r') do |io|
         commit = nil
         while line = io.gets
           line.chomp!
@@ -23,7 +25,8 @@ module GitLog
           end
           if commit.nil?
             # commit line
-            commit = line.split(';')
+            s,d,m = line.split(';')
+            commit = {sha: s, date: Time.parse(d), message: m}
           else
             # filename
             filename = line
@@ -35,13 +38,28 @@ module GitLog
         io.close
       end
       break if none
-      skip += n
+      skip += per_skip
     end
     db
   end
   
-  def self.history(path) # date yyyy-mm-dd
-    # TODO: bound date
-    # `git log $path --oneline`.split("\n")
+  # path のファイルに対して、全てのコミットログを取得する。
+  # @return: [{sha, date, message}, ...]
+  def self.history(path, options)
+    `git log --oneline --pretty=format:'%H;%cd;%s' $path #{self.format_options(options)}`.split("\n") \
+      .reject{|l| l.empty }.map{|l| s,d,m = l.split(';'); {sha: s, date: Time.parse(d), message: m}}
   end
+
+  def self.format_options(hash, ignore = {})
+    str = ''
+    str << "-n #{hash[:n]} " if hash[:n] && !ignore[:n]
+    str << "--after #{self.date_format(hash[:after])} " if hash[:after] && !ignore[:after]
+    return str
+  end
+  private_class_method :format_options
+
+  def self.date_format(date) # date yyyy-mm-dd
+    "%04d-%02d-%02d"%[date.year, date.month, date.day]
+  end
+  private_class_method :date_format
 end
