e18a552079e94b379f61888c035b913e705adc2c
舞葉 <japanjetone@live.jp>
2019-11-04 21:16:55 +0900

    Refactoring (#53)

--- src/cpp/math/method/prime/generatedivisor.cpp
+++ src/cpp/math/method/prime/generatedivisor.cpp
@@ -2,26 +2,32 @@
 // 約数列挙
 //
 // %overview
+// 約数を列挙してvectorに格納する。
+// ソートはされていない。
 //
 // %usage
+// 
+// %require
+// ```
+#include <vector>
+// ```
+// %verified
+// 10..20
+// 
+// %references
 //
 // %words
 // divisor
-// %verified
-//
 
-vector<int> generateDivisor(int number) {
-  vector<int> divs;
-  // divs.reserve(2000);
-  {
-    int d = 1;
-    while (d <= M / d) {
-      if (M / d * d == M) {
-        divs.push_back(d);
-        divs.push_back(M / d);
-      }
-      ++d;
+std::vector<long long> generateDivisor(long long n) {
+  std::vector<long long> divs;
+  for (long long d = 1; d <= n / d; ++d) {
+    if (n / d * d == n) {
+      divs.push_back(d);
+      divs.push_back(n / d);
     }
   }
+  if (divs.size() >= 2 && divs[divs.size()-1] == divs[divs.size()-2])
+    divs.pop_back();
   return divs;
 }
--- src/cpp/math/method/prime/primesieve_dynamic.cpp
+++ src/cpp/math/method/prime/primesieve_dynamic.cpp
@@ -3,11 +3,9 @@
 //
 // %overview
 // エラトステネスの篩
+// TODO: primesieve.cpp を動的にする
 //
 // %usage
-// vector<int> gen_primelist(ll high);
-// ; high : 上限
-// ; `out[i]==1`ならばiは!!素数!! (i<=1は未定義)
 //
 // %verified
 //
@@ -16,19 +14,4 @@
 //
 
 ;
-// deprecated
-vector<int> gen_primelist(ll high) {
-  vector<int> out;
-  out.resize(high + 1);
-  out[0] = out[1] = 1;
-  for (ll i = 2; i * i <= high; i++) {
-    if (out[i] == 0) {
-      for (ll j = i * i; j <= high; j += i) {  // i*i
-        out[j] = 1;
-      }
-    }
-  }
-  for (auto& x : out)
-    out ^= 1;
-  return out;
-}
+// TODO!!
