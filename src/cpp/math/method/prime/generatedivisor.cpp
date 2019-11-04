4065ae7ebbf3056eb866ddf9f0bb39d785cc3141
mai-w <mai@ukibune.net>
2019-11-04 12:18:44 +0000

    refactor by Github Actions

--- src/cpp/math/method/prime/generatedivisor.cpp
+++ src/cpp/math/method/prime/generatedivisor.cpp
@@ -6,14 +6,14 @@
 // ソートはされていない。
 //
 // %usage
-// 
+//
 // %require
 // ```
 #include <vector>
 // ```
 // %verified
 // 10..20
-// 
+//
 // %references
 //
 // %words
@@ -27,7 +27,7 @@ std::vector<long long> generateDivisor(long long n) {
       divs.push_back(n / d);
     }
   }
-  if (divs.size() >= 2 && divs[divs.size()-1] == divs[divs.size()-2])
+  if (divs.size() >= 2 && divs[divs.size() - 1] == divs[divs.size() - 2])
     divs.pop_back();
   return divs;
 }
