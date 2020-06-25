a41c7ac8fb0c5889a47f043feac0bda08b9feb81
buyoh <japanjetone@live.jp>
2020-06-25 23:30:05 +0900

    implment multi jobs to buildtest

--- tools/buildtest.rb
+++ tools/buildtest.rb
@@ -1,46 +1,84 @@
 Dir.chdir __dir__
+require 'fileutils'
 require 'optparse'
 require './dbhelper/collector.rb'
 
 require './test/test.rb'
 
 @filter = nil
+@jobs = 1
 optparser = OptionParser.new
 optparser.on('--filter regexp'){|e| @filter = Regexp.new(e) }
+optparser.on('-j', '--jobs number') do |e|
+  e = e.to_i
+  abort 'invalid arguments(jobs)' if e < 1
+  @jobs = e
+end
 # optparser.on('--log-path path'){|e| @log_path = e }
 optparser.parse!(ARGV)
 
 
-@tempdir = '/tmp'
+@tempdir = '/tmp/codelib2'
 
 # logio = @log_path ? open(@log_path, 'w') : nil
 
+def do_job(path, tid = 0)
+  return true if @filter && !(path =~ @filter)
+  puts "test: #{path}"
+
+  lang = path.split('/')[1]
+  unless Test.const_defined?(lang.upcase)
+    puts 'not implemented language: ' + lang
+    return true
+  end
+
+  tempdir = @tempdir + tid.to_s
+  FileUtils.mkdir_p tempdir unless Dir.exist? tempdir
+  langc = Test.const_get(lang.upcase)
+  tester = langc.new(path, tempdir)
+
+  result = tester.test_compilable
+  if result['default']
+    if result.all?{|k,v| v}
+      puts 'ok'
+    else
+      puts 'partially ok'
+    end
+    return true
+  else
+    puts 'failed!!'
+    return false
+  end
+
+end
+
 failed = false
 Dir.chdir('../') do
-  Dir.mkdir @tempdir unless Dir.exist? @tempdir
 
-  Document.src_files.each do |path|
-    next if @filter && !(path =~ @filter)
-    puts "test: #{path}"
-    lang = path.split('/')[1]
-    unless Test.const_defined?(lang.upcase)
-      puts 'not implemented language: ' + lang
-      next
+  if @jobs == 1
+    Document.src_files.each do |path|
+      failed |= do_job(path)
     end
-    langc = Test.const_get(lang.upcase)
-    tester = langc.new(path, @tempdir)
-    result = tester.test_compilable
-    if result['default']
-      if result.all?{|k,v| v}
-        puts 'ok'
-      else
-        puts 'partially ok'
+  else
+    mtx = Mutex.new
+    paths = Document.src_files.clone
+    pop_paths = lambda do
+      res = nil
+      mtx.synchronize do
+        res = paths.shift unless paths.empty?
       end
-    else
-      puts 'failed!!'
-      failed = true
+      next res
     end
+
+    @jobs.times.map do |tid|
+      Thread.new(tid) do |tid|
+        while path = pop_paths.call()
+          failed |= !(do_job(path, tid))
+        end
+      end
+    end.each{|t| t.join}
   end
+
 end
 
 # logio.close if logio
