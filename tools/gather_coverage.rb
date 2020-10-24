b247759dc925d5b33c019034488214f5c673c04f
buyoh <japanjetone@live.jp>
2020-10-24 17:11:57 +0900

    add tools lib collection
    remove coverage annd move impl to lib

--- tools/gather_coverage.rb
+++ tools/gather_coverage.rb
@@ -5,6 +5,7 @@
 
 require 'pathname'
 require 'json'
+require_relative './lib/collector/collection.rb'
 require_relative './lib/collector/collector.rb'
 require_relative './lib/code/codeparser.rb'
 
@@ -15,26 +16,6 @@ end
 @codelib_path = __dir__ + '/..'
 @codelib_path = File.absolute_path(@codelib_path)
 
-src_paths_path = Collector.src_paths(@codelib_path).map { |e| relative_path(e, @codelib_path + '/src')}
+collection = Collection.new(@codelib_path)
 
-coverages = {}
-src_paths_path.each do |path|
-  coverages[path] = {
-    tested_by: []
-  }
-end
-
-Collector.test_paths(@codelib_path).each do |test_path|
-  nom_test_path = relative_path(test_path, @codelib_path + '/src')
-  dfs = lambda do |path|
-    code = open(path, 'r') { |io| Code.read_code(io, Collector.lang_from_path(path))}
-    code[:dependencies].each do |dep_path|
-      nom_dep_path = relative_path(dep_path, @codelib_path + '/src')
-      coverages[nom_dep_path][:tested_by] << nom_test_path if coverages[nom_dep_path]
-      dfs.call(nom_dep_path)
-    end
-  end
-  dfs.call(nom_test_path)
-end
-
-puts JSON.pretty_generate(coverages)
+puts JSON.pretty_generate(collection.src_relations)
