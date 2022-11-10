68099ce9166ddf3f370ce50c82e64bc0e27ffce8
buyoh <japanjetone@live.jp>
2022-11-11 02:51:51 +0900

    Add assertion

--- src/cpp/grid/datastructure/euclid.hpp
+++ src/cpp/grid/datastructure/euclid.hpp
@@ -95,16 +95,54 @@ struct F {
 
   explicit F(int h, int w) : height(h), width(w), data(h * w) {}
   F() : F(1, 1) {}
+#if 0
+  void assert_safe(int y, int x) const {
+    if (!safe(y, x)) {
+      clog << "assertion failed: field=" << height << "x" << width
+           << ": try=" << y << "," << x << endl;
+      assert(safe(y, x));
+    }
+  }
+  inline T &operator()(int y, int x) {
+    assert_safe(y, x);
+    return data[x + y * width];
+  }
+  inline T &operator()(P p) {
+    assert_safe(p);
+    return data[p.x + p.y * width];
+  }
+  inline T operator()(int y, int x) const {
+    assert_safe(y, x);
+    return data[x + y * width];
+  }
+  inline T operator()(P p) const {
+    assert_safe(p);
+    return data[p.x + p.y * width];
+  }
+#else
+  inline T& operator()(int y, int x) {
+    return data[x + y * width];
+  }
+  inline T& operator()(P p) {
+    return data[p.x + p.y * width];
+  }
+  inline T operator()(int y, int x) const {
+    return data[x + y * width];
+  }
+  inline T operator()(P p) const {
+    return data[p.x + p.y * width];
+  }
+#endif
+  inline bool safe(int y, int x) const {
+    return 0 <= y && y < height && 0 <= x && x < width;
+  }
+  inline bool safe(P p) const {
+    return 0 <= p.y && p.y < height && 0 <= p.x && p.x < width;
+  }
 
-  inline T& operator()(int y, int x) { return data[x + y * width]; }
-  inline T& operator()(P p) { return data[p.x + p.y * width]; }
-  inline T operator()(int y, int x) const { return data[x + y * width]; }
-  inline T operator()(P p) const { return data[p.x + p.y * width]; }
-
-  inline bool safe(int y, int x) const { return 0 <= y && y < height && 0 <= x && x < width; }
-  inline bool safe(P p) const { return 0 <= p.y && p.y < height && 0 <= p.x && p.x < width; }
-
-  inline void fill(T e) { std::fill(data.begin(), data.end(), e); }
+  inline void fill(T e) {
+    std::fill(data.begin(), data.end(), e);
+  }
   inline void resize(int h, int w) {
     height = h;
     width = w;
